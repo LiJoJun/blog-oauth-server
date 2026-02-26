106f2c69643a45cf187c64042c4ea4d3fe094691
LiJoJun <102477199+LiJoJun@users.noreply.github.com>
2026-02-26 10:05:04 +0800

    Add OAuth handler for GitHub access token

---
+++ api/oauth.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code
+    })
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
